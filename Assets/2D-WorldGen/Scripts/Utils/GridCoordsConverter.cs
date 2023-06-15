bdb9c76ef32aba8d8333f2a545ed8a186799579e
Gugorrex <62066486+Gugorrex@users.noreply.github.com>
2023-06-15 21:57:38 +0200

    migrated GridCoordsConverter from world gen v2

---
+++ Assets/2D-WorldGen/Scripts/Utils/GridCoordsConverter.cs
@@ -0,0 +1,41 @@
+﻿using Unity.Mathematics;
+
+namespace _2D_WorldGen.Scripts.Utils
+{
+    public static class GridCoordsConverter // TODO implement in legacy code
+    {
+        public static int2 WorldToSubGridCoords(int subGridSize, int2 worldCoords, out int2 localSubCoords)
+        {
+            var subCoords = int2.zero;
+            localSubCoords = int2.zero;
+
+            subCoords.x = worldCoords.x >= 0 ? worldCoords.x / subGridSize : ((worldCoords.x + 1) / subGridSize) - 1;
+            subCoords.y = worldCoords.y >= 0 ? worldCoords.y / subGridSize : ((worldCoords.y + 1) / subGridSize) - 1;
+
+            localSubCoords.x = worldCoords.x >= 0
+                ? worldCoords.x % subGridSize
+                : worldCoords.x + (-subCoords.x * subGridSize);
+            localSubCoords.y = worldCoords.y >= 0
+                ? worldCoords.y % subGridSize
+                : worldCoords.y + (-subCoords.y * subGridSize);
+            
+            return subCoords;
+        }
+
+        public static int2 SubToWorldGridCoords(int subGridSize, int2 subCoords)
+        {
+            var localSubCoords = int2.zero;
+            return SubToWorldGridCoords(subGridSize, subCoords, localSubCoords);
+        }
+        
+        public static int2 SubToWorldGridCoords(int subGridSize, int2 subCoords, int2 localSubCoords)
+        {
+            var worldCoords = int2.zero;
+
+            worldCoords.x = subCoords.x * subGridSize + localSubCoords.x;
+            worldCoords.y = subCoords.y * subGridSize + localSubCoords.y;
+
+            return worldCoords;
+        }
+    }
+}
